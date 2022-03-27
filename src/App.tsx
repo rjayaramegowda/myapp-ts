3e219c1cb1f4af8f1c53be2951000671351f73cb
Ravichandran Jayaramegowda <50406692+rjayaramegowda@users.noreply.github.com>
2022-03-27 18:57:15 +0530

    npx create-react-app --template typescript

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container">
+     <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+    </div>
+  );
+}
+
+export default App;
