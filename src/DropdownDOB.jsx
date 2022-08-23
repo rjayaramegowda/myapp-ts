20e4acc0c61aa498346d4677a0bc8125acc4bb24
Ravichandran Jayaramegowda <50406692+rjayaramegowda@users.noreply.github.com>
2022-08-23 13:57:41 +0530

    Update DropdownDOB.jsx

--- src/DropdownDOB.jsx
+++ src/DropdownDOB.jsx
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Col, FormControl, Row } from "react-bootstrap";
 
-import en from 'date-fns/locale/en-GB';
-import 'react-datepicker/dist/react-datepicker.css';
-import { format, setMonth } from 'date-fns';
+import en from "date-fns/locale/en-GB";
+import "react-datepicker/dist/react-datepicker.css";
+import { format, setMonth } from "date-fns";
 
 const DropdownDOB = () => {
   const [today, setToday] = useState(new Date());
-  const [dob, setDob] = useState(today.toISOString().split("T")[0]);
+  const [dob, setDob] = useState();
 
   const months = new Array(12).fill(null).map((_, i) => ({
     value: i,
-    label: format(setMonth(new Date(), i), 'MMMM', {
+    label: format(setMonth(new Date(), i), "MMMM", {
       locale: en,
     }),
   }));
@@ -22,51 +22,59 @@ const DropdownDOB = () => {
   const handleYearChange = ({ target: { value } }) => {
     setToday(new Date(today.setFullYear(value)));
     setDob(today.toISOString().split("T")[0]);
-  }
+  };
 
   const handleMonthChange = ({ target: { value } }) => {
     setToday(new Date(today.setMonth(value)));
     setDob(today.toISOString().split("T")[0]);
-  }
+  };
 
-  const dateHandler = ({target: {value}}) => setDob(value);
+  const handleDateChange = ({ target: { value } }) => {
+    setToday(new Date(today.setDate(value)));
+    setDob(today.toISOString().split("T")[0]);
+  };
+
+  const dateHandler = ({ target: { value } }) => setDob(value);
 
   return (
-    <Row className="g-1 my-5" style={{maxWidth:400}}>
-
-        <Col  sm="5">
-        <select className='form-select' onChange={handleMonthChange}>
-        {months.map(({ value, label }) => (
-          <option value={value} key={value}>
-            {label}
-          </option>
-        ))}
-      </select>
-        </Col>
-        <Col  sm="3">
-        <select className='form-select'>
-        {days.map(day => (
-          <option value={day} key={day}>
-            {day}
-          </option>
-        ))}
-      </select>
-        </Col>
-        <Col  sm="4">
-        <select className='form-select' onChange={handleYearChange}>
-        {years.map(year => (
-          <option value={year} key={year}>
-            {year}
-          </option>
-        ))}
-      </select>
-        </Col>
-        <Col  sm="12 mt-2">
-          <FormControl value={dob} type="date" name="dob" onChange={dateHandler} />
-          </Col>
-      </Row>
+    <Row className="g-1 my-5" style={{ maxWidth: 400 }}>
+      <Col sm="5">
+        <select className="form-select" onChange={handleMonthChange}>
+          {months.map(({ value, label }) => (
+            <option value={value} key={value}>
+              {label}
+            </option>
+          ))}
+        </select>
+      </Col>
+      <Col sm="3">
+        <select className="form-select" onChange={handleDateChange}>
+          {days.map((day) => (
+            <option value={day} key={day}>
+              {day}
+            </option>
+          ))}
+        </select>
+      </Col>
+      <Col sm="4">
+        <select className="form-select" onChange={handleYearChange}>
+          {years.map((year) => (
+            <option value={year} key={year}>
+              {year}
+            </option>
+          ))}
+        </select>
+      </Col>
+      <Col sm="12 mt-2">
+        <FormControl
+          value={dob}
+          type="date"
+          name="dob"
+          onChange={dateHandler}
+        />
+      </Col>
+    </Row>
   );
 };
-
 
 export default DropdownDOB;
