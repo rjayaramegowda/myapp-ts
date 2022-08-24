4d6928bc39f87e24207cb775c1b5130844cd9d80
Ravichandran Jayaramegowda <50406692+rjayaramegowda@users.noreply.github.com>
2022-08-24 17:40:20 +0530

    added react-select-datepicker

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,58 @@
-import React, { useEffect } from "react";
-import { Button, Container } from "react-bootstrap";
+import React, { useState, useCallback, useEffect } from "react";
+import { Button, Col, Container, FormControl, Row } from "react-bootstrap";
 import DropdownDOB from "./DropdownDOB";
+import { SelectDatepicker } from "react-select-datepicker";
 
 function App() {
+  const [value, setValue] = useState<Date | null>();
+
+  // const onDateChange = useCallback((date: Date) => {
+  //   setValue(date);
+  // }, []);
+
   useEffect(() => {
-    console.log("init()");
+    document.querySelector(".rsd__react-select-datepicker")?.classList.add("gap-2");
+    document.querySelector(".rsd__select-day")?.classList.add("form-select");
+    document.querySelector(".rsd__select-month")?.classList.add("form-select");
+    document.querySelector(".rsd__select-year")?.classList.add("form-select");
   }, []);
+
+  function onDateChange(date: Date | null) {
+    if (date) {
+      setValue(date)
+    }
+  }
 
   return (
     <Container>
-      <h1>React Bootstrap</h1>
       <form action="submit-form.php">
-        <DropdownDOB
-          value="2021-05-07"
-          min="2020-05-07"
-          max="2022-05-07"
-          name="dateOfBirth"
-        />
-        <Button type="submit">Submit</Button>
+        <Row>
+          <Col md="12">
+            <h1>React Bootstrap</h1>
+            <DropdownDOB
+              value="2021-05-07"
+              min="2020-05-07"
+              max="2022-05-07"
+              name="dateOfBirth"
+            />
+          </Col>
+          <Col md="12">
+            <SelectDatepicker
+              maxDate={new Date("1997-07-23T18:30:00.000Z")}
+              minDate={new Date("1987-01-13T18:30:00.000Z")}
+              hideLabels
+              selectedDate={value}
+              onDateChange={onDateChange}
+            />
+          </Col>
+          <Col md="12">
+            <FormControl className="d-none" required type="date" name="dob" value={value?.toISOString().split("T")[0] || ''} />
+            </Col>
+
+          <Col className="mt-5" md="12">
+            <Button type="submit">Submit</Button>
+          </Col>
+        </Row>
       </form>
     </Container>
   );
