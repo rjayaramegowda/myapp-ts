95e38d36ff7869d5b96063bb829c5350244ad684
Ravichandran Jayaramegowda <50406692+rjayaramegowda@users.noreply.github.com>
2022-08-28 00:17:23 +0530

    added FlightDatePicker

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,56 @@
-import React, { useEffect } from "react";
+import { Tab, Tabs } from "@mui/material";
+import React, { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
+import FlightIcon from "@mui/icons-material/Flight";
+import HotelIcon from "@mui/icons-material/Hotel";
+import DirectionsCarIcon from "@mui/icons-material/DirectionsCar";
+import AsynchronousSearching from "./com/AsynchronousSearching";
 
 function App() {
   useEffect(() => {
     console.log("init()");
   }, []);
+
+  const [activeTab, setActiveTab] = useState("one");
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setActiveTab(newValue);
+  };
+
   return (
     <Container>
-      <h1>React Bootstrap</h1>
+      <Tabs
+        value={activeTab}
+        onChange={handleChange}
+        aria-label="wrapped label tabs example"
+        className="my-5"
+      >
+        <Tab
+          icon={<FlightIcon />}
+          className="w-100 text-capitalize"
+          value="one"
+          label="Flights"
+          wrapped
+        />
+        <Tab
+          icon={<HotelIcon />}
+          className="w-100 text-capitalize"
+          value="two"
+          label="Hotels"
+        />
+        <Tab
+          icon={<DirectionsCarIcon />}
+          className="w-100 text-capitalize"
+          value="three"
+          label="Cars"
+        />
+      </Tabs>
+
+      <h1>react-bootstrap-typeahead</h1>
+      <AsynchronousSearching />
+
+      <h1 className="my-5">react-google-flight-datepicker</h1>
+      <h4>RangeDatePicker</h4>
     </Container>
   );
 }
---
+++ src/com/AsynchronousSearching.tsx
@@ -0,0 +1,61 @@
+import React, { Fragment, useState } from "react";
+import { AsyncTypeahead } from "react-bootstrap-typeahead";
+import "react-bootstrap-typeahead/css/Typeahead.css";
+import { isAnyArrayBuffer } from "util/types";
+
+const SEARCH_URI = "https://api.github.com/search/users";
+
+const AsynchronousSearching = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [options, setOptions] = useState([]);
+
+  const handleSearch = (query: any) => {
+    setIsLoading(true);
+
+    fetch(`${SEARCH_URI}?q=${query}+in:login&page=1&per_page=50`)
+      .then((resp) => resp.json())
+      .then(({ items }) => {
+        const options = items.map((i: any) => ({
+          avatar_url: i.avatar_url,
+          id: i.id,
+          login: i.login,
+        }));
+
+        setOptions(options);
+        setIsLoading(false);
+      });
+  };
+
+  // Bypass client-side filtering by returning `true`. Results are already
+  // filtered by the search endpoint, so no need to do it again.
+  const filterBy = () => true;
+
+  return (
+    <AsyncTypeahead
+      filterBy={filterBy}
+      id="async-example"
+      isLoading={isLoading}
+      labelKey="login"
+      minLength={3}
+      onSearch={handleSearch}
+      options={options}
+      placeholder="Search for a Github user..."
+      renderMenuItemChildren={(option: any, props) => (
+        <Fragment>
+          <img
+            alt={option.login}
+            src={option.avatar_url}
+            style={{
+              height: "24px",
+              marginRight: "10px",
+              width: "24px",
+            }}
+          />
+          <span>{option.login}</span>
+        </Fragment>
+      )}
+    />
+  );
+};
+
+export default AsynchronousSearching;
